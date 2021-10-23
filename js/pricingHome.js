b86e4f9c7eaad28e6a3d650d5a0d902564dd4cf6
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-23 20:14:46 +0200

    monthly and annually price toggle finished

--- js/pricingHome.js
+++ js/pricingHome.js
@@ -1,5 +1,22 @@
-const toggle = document.querySelector("#input");
+const toggleDiv = document.querySelector(".switch");
+const toggleInput = document.querySelector("#input");
+const basicPrice = document.querySelector("#basicPrice");
+const profPrice = document.querySelector("#profPrice");
+const masterPrice = document.querySelector("#masterPrice");
 
-toggle.addEventListener("click", () => {
-  toggle.classList.toggle("open");
+toggleInput.addEventListener("click", () => {
+  toggleDiv.classList.toggle("open");
+
+  if (toggleDiv.classList.contains("open")) {
+    basicPrice.innerHTML = "$199.99";
+    profPrice.innerHTML = "$399.99";
+    masterPrice.innerHTML = "$249.99";
+  } else {
+    basicPrice.innerHTML = "$19.99";
+    profPrice.innerHTML = "$39.99";
+    masterPrice.innerHTML = "$24.99";
+  }
 });
+
+// annualy === open
+// monthly === null
